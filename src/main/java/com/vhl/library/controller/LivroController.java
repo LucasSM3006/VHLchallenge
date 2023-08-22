3af336f432843ae0f6a7a5bd133dfb7b93e4e6ec
LucasSM3006 <sasso3006@gmail.com>
2023-08-22 17:41:20 -0300

    Controllers. Contém os métodos necessários, porém ainda não implementados.

--- src/main/java/com/vhl/library/controller/LivroController.java
+++ src/main/java/com/vhl/library/controller/LivroController.java
@@ -1,6 +1,9 @@
 package com.vhl.library.controller;
 
+import com.vhl.library.model.DTO.LivroDTO;
+import com.vhl.library.services.LivroService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/livro")
 public class LivroController {
 
-    @GetMapping("/listar")
-    public String listar() {
+    private final LivroService livroService;
+
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
+    @GetMapping("/adicionar")
+    public String adicionar(@RequestBody LivroDTO livroDTO) {
+        // return livroService.adicionar(livroDTO);
         return "tudo ok";
+    }
+
+    @GetMapping("/editar")
+    public String editar() {
+        // livroService.editar();
+        return "";
+    }
+
+    @GetMapping("/excluir")
+    public String excluir() {
+        // livroService.excluir(id);
+        return "";
+    }
+
+    @GetMapping("/emprestar")
+    public String emprestar() {
+        return "";
+    }
+
+    @GetMapping("/devolver")
+    public String devolver() {
+        return "";
+    }
+
+    @GetMapping("/pesquisarPorTitulo")
+    public String pesquisarPorTitulo() {
+        return "";
+    }
+
+    @GetMapping("/pesquisarPorAutor")
+    public String pesquisarPorAutor() {
+        return "";
     }
 }
--- src/main/java/com/vhl/library/controller/UsuarioController.java
+++ src/main/java/com/vhl/library/controller/UsuarioController.java
@@ -1,5 +1,11 @@
 package com.vhl.library.controller;
 
+import com.vhl.library.model.DTO.LivroDTO;
+import com.vhl.library.model.DTO.UsuarioDTO;
+import com.vhl.library.services.LivroService;
+import com.vhl.library.services.UsuarioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/usuario")
 public class UsuarioController {
 
+    private final UsuarioService usuarioService;
 
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @GetMapping("/adicionar")
+    public String adicionar(@RequestBody UsuarioDTO usuarioDTO) {
+        // return usuarioService.adicionar(usuarioDTO);
+        return "tudo ok";
+    }
+
+    @GetMapping("/editar")
+    public String editar() {
+        // return usuarioService.editar();
+        return "";
+    }
+
+    @GetMapping("/excluir")
+    public String excluir() {
+        // return usuarioService.excluir(id);
+        return "";
+    }
 }
