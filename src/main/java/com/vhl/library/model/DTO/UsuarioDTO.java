5277cb910a46eb00343c794d81e8b6487200589d
LucasSM3006 <sasso3006@gmail.com>
2023-08-23 19:38:36 -0300

    Adicionado getters e setters.

--- src/main/java/com/vhl/library/model/DTO/UsuarioDTO.java
+++ src/main/java/com/vhl/library/model/DTO/UsuarioDTO.java
@@ -2,4 +2,12 @@ package com.vhl.library.model.DTO;
 
 public class UsuarioDTO {
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
