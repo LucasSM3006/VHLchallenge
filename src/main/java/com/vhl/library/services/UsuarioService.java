cd5983b864f942662cdd593c8fd923de882e6482
LucasSM3006 <sasso3006@gmail.com>
2023-08-24 18:06:18 -0300

    Métodos de pesquisa criados e implementados. Pequena checagem quando não vem nenhum resultado, retornando assim uma lista contendo um usuário sem informações.

--- src/main/java/com/vhl/library/services/UsuarioService.java
+++ src/main/java/com/vhl/library/services/UsuarioService.java
@@ -1,12 +1,15 @@
 package com.vhl.library.services;
 
+import com.vhl.library.model.DTO.LivroDTO;
 import com.vhl.library.model.DTO.UsuarioDTO;
+import com.vhl.library.model.Livro;
 import com.vhl.library.model.Usuario;
 import com.vhl.library.repos.UsuarioRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,8 +21,9 @@ public class UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
 
-    public ResponseEntity<UsuarioDTO> adicionar(UsuarioDTO usuarioDTO) {
-        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
+    public ResponseEntity<Usuario> adicionar(UsuarioDTO usuarioDTO) {
+//        return new ResponseEntity<>(usuarioDTO, HttpStatus.OK);
+    return null;
     }
 
     public String editar() {
@@ -30,8 +34,33 @@ public class UsuarioService {
         return null;
     }
 
-    public ResponseEntity<List<Usuario>> listar(UsuarioDTO usuarioDTO) {
+    public ResponseEntity<List<UsuarioDTO>> pesquisarPorNome(UsuarioDTO usuarioDTO) {
         List<Usuario> usuarios = usuarioRepository.findByNomeContainingIgnoreCase(usuarioDTO.getNome());
-        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+        List<UsuarioDTO> usuariosDTO = convertToDTO(usuarios);
+
+        if(usuarios.isEmpty()) {
+            UsuarioDTO usuarioVazio = new UsuarioDTO();
+            usuarioVazio.setId(0L);
+            usuarioVazio.setNome("Sem resultados.");
+            usuariosDTO.add(usuarioVazio);
+        }
+
+        return new ResponseEntity<>(usuariosDTO, HttpStatus.OK);
+    }
+
+    private List<UsuarioDTO> convertToDTO(List<Usuario> usuarios) {
+        List<UsuarioDTO> usuariosDTO = new ArrayList<>();
+
+        if(usuarios.isEmpty()) return usuariosDTO;
+
+        for(Usuario usuario : usuarios) {
+            UsuarioDTO usuarioDTO = new UsuarioDTO();
+            usuarioDTO.setId(usuario.getId());
+            usuarioDTO.setNome(usuario.getNome());
+
+            usuariosDTO.add(usuarioDTO);
+        }
+
+        return usuariosDTO;
     }
 }
