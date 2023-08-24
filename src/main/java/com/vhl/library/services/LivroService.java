2eec8dab31857a8611ea91d58a8b7ceaa81f810e
LucasSM3006 <sasso3006@gmail.com>
2023-08-24 18:04:25 -0300

    Métodos de pesquisa criados e implementados. Pequena checagem quando não vem nenhum resultado, retornando assim uma lista contendo um livro sem informações.

--- src/main/java/com/vhl/library/services/LivroService.java
+++ src/main/java/com/vhl/library/services/LivroService.java
@@ -1,11 +1,13 @@
 package com.vhl.library.services;
 
 import com.vhl.library.model.DTO.LivroDTO;
+import com.vhl.library.model.Livro;
 import com.vhl.library.repos.LivroRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -29,15 +31,55 @@ public class LivroService {
         return new ResponseEntity<>("Ok.", HttpStatus.OK);
     }
 
-    public ResponseEntity<List<LivroDTO>> pesquisarPorAutor() {
-        return null;
+    public ResponseEntity<List<LivroDTO>> pesquisarPorAutor(LivroDTO livroDTO) {
+        List<Livro> livros = livroRepository.findByAutorContainingIgnoreCase(livroDTO.getAutor());
+        List<LivroDTO> livrosDTO = convertToDTO(livros);
+        isEmpty(livrosDTO);
+
+        return new ResponseEntity<>(livrosDTO, HttpStatus.OK);
     }
 
-    public ResponseEntity<List<LivroDTO>> pesquisarPorTitulo() {
-        return null;
+    public ResponseEntity<List<LivroDTO>> pesquisarPorTitulo(LivroDTO livroDTO) {
+        List<Livro> livros = livroRepository.findByTituloContainingIgnoreCase(livroDTO.getTitulo());
+        List<LivroDTO> livrosDTO = convertToDTO(livros);
+        isEmpty(livrosDTO);
+
+        return new ResponseEntity<>(livrosDTO, HttpStatus.OK);
     }
 
-    public ResponseEntity<List<LivroDTO>> pesquisarPorTituloEAutor() {
-        return null;
+    public ResponseEntity<List<LivroDTO>> pesquisarPorTituloEAutor(LivroDTO livroDTO) {
+        List<Livro> livros = livroRepository.findByAutorContainingIgnoreCaseAndTituloContainingIgnoreCase(livroDTO.getAutor(), livroDTO.getTitulo());
+        List<LivroDTO> livrosDTO = convertToDTO(livros);
+        isEmpty(livrosDTO);
+
+        return new ResponseEntity<>(livrosDTO, HttpStatus.OK);
+    }
+
+    private List<LivroDTO> isEmpty(List<LivroDTO> livrosDTO) {
+        if(livrosDTO.isEmpty()) {
+            LivroDTO livroVazio = new LivroDTO();
+            livroVazio.setId(0L);
+            livroVazio.setAutor("Sem resultados.");
+            livrosDTO.add(livroVazio);
+        }
+
+        return livrosDTO;
+    }
+
+    private List<LivroDTO> convertToDTO(List<Livro> livros) {
+        List<LivroDTO> livrosDTO = new ArrayList<>();
+
+        if(livros.isEmpty()) return livrosDTO;
+
+        for(Livro livro : livros) {
+            LivroDTO livroDTO = new LivroDTO();
+            livroDTO.setId(livro.getId());
+            livroDTO.setAutor(livro.getAutor());
+            livroDTO.setTitulo(livro.getTitulo());
+
+            livrosDTO.add(livroDTO);
+        }
+
+        return livrosDTO;
     }
 }
