2756a24138cdda7c51cf46dabb2191977f6e34b4
LucasSM3006 <sasso3006@gmail.com>
2023-08-26 19:13:30 -0300

    Métodos para edição. Lógica de exclusão também.

--- src/main/java/com/vhl/library/repos/UsuarioRepository.java
+++ src/main/java/com/vhl/library/repos/UsuarioRepository.java
@@ -1,10 +1,15 @@
 package com.vhl.library.repos;
 
 import com.vhl.library.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
-    List<Usuario> findByNomeContainingIgnoreCase(String nome);
+
+    List<Usuario> findByNomeContainingIgnoreCaseAndExcluidoIsFalse(String nome);
+
+    Optional<Usuario> findByIdAndExcluidoFalse(Integer id);
 }
