ebb676c9b27c7abbe84c198e022425891a244458
LucasSM3006 <sasso3006@gmail.com>
2023-08-26 19:26:12 -0300

    Todas as pesquisas agora tem "ExcluidoIsFalse", por causa da mudança de lógica.

--- src/main/java/com/vhl/library/repos/LivroRepository.java
+++ src/main/java/com/vhl/library/repos/LivroRepository.java
@@ -4,9 +4,12 @@ import com.vhl.library.model.Livro;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LivroRepository extends CrudRepository<Livro, Integer> {
-    List<Livro> findByAutorContainingIgnoreCase(String autor);
-    List<Livro> findByTituloContainingIgnoreCase(String titulo);
-    List<Livro> findByAutorContainingIgnoreCaseAndTituloContainingIgnoreCase(String autor, String titulo);
+    List<Livro> findByAutorContainingIgnoreCaseAndExcluidoIsFalse(String autor);
+    List<Livro> findByTituloContainingIgnoreCaseAndExcluidoIsFalse(String titulo);
+    List<Livro> findByAutorContainingIgnoreCaseAndTituloContainingIgnoreCaseAndExcluidoIsFalse(String autor, String titulo);
+
+    Optional<Livro> findByIdAndExcluidoFalse(Integer id);
 }
