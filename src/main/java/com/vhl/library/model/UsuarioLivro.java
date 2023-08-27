88c664af0613568ff2986f1cb3884aeb7ccc0404
LucasSM3006 <sasso3006@gmail.com>
2023-08-27 01:27:06 -0300

    Métodos getters e setters adicionados.

--- src/main/java/com/vhl/library/model/UsuarioLivro.java
+++ src/main/java/com/vhl/library/model/UsuarioLivro.java
@@ -20,4 +20,36 @@ public class UsuarioLivro {
     private Livro livro;
 
     private boolean ativo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
 }
