99bc282ded7f6639971b00cb721ce49e6549decc
LucasSM3006 <sasso3006@gmail.com>
2023-08-27 01:31:19 -0300

    Métodos de busca adicionados.

--- src/main/java/com/vhl/library/repos/UsuarioLivroRepository.java
+++ src/main/java/com/vhl/library/repos/UsuarioLivroRepository.java
@@ -1,7 +1,19 @@
 package com.vhl.library.repos;
 
 import com.vhl.library.model.UsuarioLivro;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UsuarioLivroRepository extends CrudRepository<UsuarioLivro, Integer> {
+
+    @Query("SELECT ul FROM UsuarioLivro ul WHERE ul.usuario.id = :usuarioId AND ul.livro.id = :livroId AND ul.ativo = true")
+    List<UsuarioLivro> findActiveUsuarioLivrosByUsuarioIdAndLivroId(
+            @Param("usuarioId") Integer usuarioId,
+            @Param("livroId") Integer livroId
+    );
+
+    List<UsuarioLivro> findByAtivoTrueAndUsuarioId(@Param("usuarioId") Integer usuarioId);
 }
